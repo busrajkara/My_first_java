06477ff7e9e7193dd613decbd6f13be7919828d5
Büşra KARA <b.kara@media.local>
2024-09-10 09:53:20 +0300

    my first java code

---
+++ src/AnaClass.java
@@ -0,0 +1,10 @@
+public class AnaClass {
+    public static void main(String[] args) {
+        String str = "merhaba";
+        String isim = "Büşra";
+        String mesaj ;
+        mesaj = str+ " "+ isim;
+
+        System.out.println(mesaj);
+    }
+}
